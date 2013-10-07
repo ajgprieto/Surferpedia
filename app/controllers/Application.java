2bf8e0fc64de4cb61cf5aa442468965af23fadb4
scotthonda <scotthonda@scott-hondas-MacBook-Pro.local>
2013-10-06 21:20:54 -1000

    Initial Commit of sorce code

---
+++ app/controllers/Application.java
@@ -0,0 +1,59 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.Index;
+import views.html.Page1;
+import views.html.koloheandino;
+import views.html.keliamoniz;
+import views.html.noa;
+
+/**
+ * Implements the controllers for this application.
+ */
+public class Application extends Controller {
+
+  /**
+   * Returns the home page. 
+   * @return The resulting home page. 
+   */
+  public static Result index() {
+    return ok(Index.render("Welcome to the home page."));
+  }
+  
+  /**
+   * Returns page1, a simple example of a second page to illustrate navigation.
+   * @return The Page1.
+   */
+  public static Result page1() {
+    return ok(Page1.render("Welcome to Page1."));
+    
+  }
+ 
+  /**
+   * Returns page1, a simple example of a second page to illustrate navigation.
+   * @return The Page1.
+   */
+  public static Result koloheandino() {
+    return ok(koloheandino.render("Welcome to Page1."));
+    
+  }
+  
+  /**
+   * Returns page1, a simple example of a second page to illustrate navigation.
+   * @return The Page1.
+   */
+  public static Result keliamoniz() {
+    return ok(keliamoniz.render("Welcome to Page1."));
+    
+  }
+  
+  /**
+   * Returns page1, a simple example of a second page to illustrate navigation.
+   * @return The Page1.
+   */
+  public static Result noa() {
+    return ok(noa.render("Welcome to Page1."));
+    
+  }
+}
